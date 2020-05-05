99f6a555ef32b275036eabd55f34bc4c8b2c8a5f
s0j011f <saugata.jana@walmartlabs.com>
2020-05-05 21:03:49 +0530

    Setup

---
+++ src/main/java/com/mms/ManageMySocietyApp.java
@@ -0,0 +1,11 @@
+package com.mms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageMySocietyApp {
+	public static void main(String[] args) {
+		SpringApplication.run(ManageMySocietyApp.class, args);
+	}
+}
